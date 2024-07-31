96f2ade6096e1d44b110d560856e514438aa3ec0
Andrew Kim <47552877+drewlonious@users.noreply.github.com>
2024-07-31 13:28:58 -0700

    Update 1 — merge sorted array.py
    cleans up code

--- 1 — Top Interview 150/1 — array and string/1 — merge sorted array.py
+++ 1 — Top Interview 150/1 — array and string/1 — merge sorted array.py
@@ -7,26 +7,27 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
 
-        # Two Pointers: p1 for nums1, p2 for nums2, and p for the merged array
-        p1 = m - 1  # nums1
-        p2 = n - 1  # nums2
-        p = m + n - 1  # merged array
+        # Two Pointers: p1 for nums1, p2 for nums2, and last for merged array
+        p1 = m - 1
+        p2 = n - 1
+        last = m + n - 1
 
-        # Merge elements from both end of both arrays
+        # Merge elements in reverse order
+
         while p1 >= 0 and p2 >= 0:
             if nums1[p1] > nums2[p2]:
-                nums1[p] = nums1[p1]
+                nums1[last] = nums1[p1]
                 p1 -= 1
             else:
-                nums1[p] = nums2[p2]
+                nums1[last] = nums2[p2]
                 p2 -= 1
-            p -= 1
+            last -= 1
 
         # Copy remaining elements from nums2, if any
         while p2 >= 0:
-            nums1[p] = nums2[p2]
+            nums1[last] = nums2[p2]
             p2 -= 1
-            p -= 1
+            last -= 1
 
 
 nums1 = [1, 2, 3, 0, 0, 0]
